9ccbb1544d91b46bd7f7472763d0c011a93aa3a1
Jock <jack_hammer88@hotmail.com>
2020-09-06 15:03:29 +1200

    Separated getting the data of LN into another file to prevent clutter and added code to getting multiple artist/publisher properly.

---
+++ ln_info.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
+           }
+
+
+def getLNData(url, headers):
+    link_r = requests.get(url, headers=headers)
+    print("\nStatus:", link_r.status_code)
+    link_soup = BeautifulSoup(link_r.text, "lxml")
+    link_result = link_soup.find(
+        "div", {"class": "g-cols wpb_row offset_default"})
+    link_data = link_result.find_all("div", {"class": "one-third"})
+    # use .replace("\n"," ") instead of strip to prevent no spacing in case of two artist/author/publishers
+    for data in link_data:
+        data_type = data.find("a", {"class": "genre type"}).text
+        data_author = data.find("a", {"id": "authtag"}).text
+        data_artist = data.find(
+            "div", {"id": "showartists"}).findAll("a")
+        data_licensed = data.find(
+            "div", {"id": "showlicensed"}).text.strip()
+        data_comp_tl = data.find(
+            "div", {"id": "showtranslated"}).text.strip()
+        data_ori_pub = data.find("div", {"id": "showopublisher"}).findAll("a")
+        data_eng_pub = data.find(
+            "div", {"id": "showepublisher"}).text.strip()
+        data_statuscoo = data.find(
+            "div", {"id": "editstatus"}).text.strip()
+        print("\nType:", data_type)
+        print("Author(s):", data_author)
+        if len(data_artist) > 1:
+            print("Artist(s):")
+            for artists in data_artist:
+                artist = artists.text
+                print(artist, " ")
+        print("Licensed:", data_licensed)
+        print("Completely Translated:", data_comp_tl)
+        if len(data_ori_pub) > 1:
+            print("Original Publisher(s):")
+            for opubs in data_ori_pub:
+                opub = opubs.text
+                print(opub, " ")
+        print("English Publisher:", data_eng_pub)
+        print("Status in COO:", data_statuscoo)
+
+
+getLNData('https://www.novelupdates.com/series/10-nen-goshi-no-hikiniito-o-yamete-gaishutsushitara-jitaku-goto-isekai-ni-tenishiteta/', headers)
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-
 # need to have user agent
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
            }
