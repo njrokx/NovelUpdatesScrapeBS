c464ae0724db30f828eef466656eeb031af36ded
Jock <jack_hammer88@hotmail.com>
2020-09-07 11:57:29 +1200

    Added some code for traversing the pages.

--- main.py
+++ main.py
@@ -4,21 +4,30 @@ from ln_info import getLNData
 # need to have user agent
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
            }
-starturl = 'https://www.novelupdates.com/novelslisting/?sort=7&order=1&status=1&pg=1'
-r = requests.get(
-    starturl, headers=headers)
-print("\nStatus:", r.status_code)
-soup = BeautifulSoup(r.text, "lxml")
-results = soup.find("div", {"class": "w-blog-content other"})
-links = results.find_all("div", {"class": "search_main_box_nu"})
-# print(soup.prettify())
-# print(links)
-try:
-    pages = soup.find("div", {"class": "digg_pagination"}).find(
-        "a", {"class": "next_page"}).attrs["href"]
-    print("https:" + pages)
-except:
-    print("No next page.")
+url = 'https://www.novelupdates.com/novelslisting/?sort=7&order=1&status=1&pg=1'
+
+i = 1
+while i < 10:
+    r = requests.get(url, headers=headers)
+
+    print("\nStatus:", r.status_code)
+    print("Page:", i)
+    soup = BeautifulSoup(r.text, "lxml")
+    results = soup.find("div", {"class": "w-blog-content other"})
+    links = results.find_all("div", {"class": "search_main_box_nu"})
+    # print(soup.prettify())
+    # print(links)
+    try:
+        pages = soup.find("div", {"class": "digg_pagination"}).find(
+            "a", {"class": "next_page"}).attrs["href"]
+        print("https:" + pages)
+        url = "https:"+pages
+    except:
+        print("No next page.")
+
+    i += 1
+# db path for running the mongodb server
+# "C:\Program Files\MongoDB\Server\4.4\bin\mongod.exe" --dbpath G:\mongodb\data
 
 # for item in links:
 #     item_title = item.find("a").text
