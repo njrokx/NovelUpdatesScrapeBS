a25957b09a10f72375618456301cb68e5a128466
Jock <jack_hammer88@hotmail.com>
2020-09-10 11:30:40 +1200

    Fixed some code dealing with multiple artist/author/publisher displaying incorrectly as well as adding code for storing the data in Mongodb.

--- ln_info.py
+++ ln_info.py
@@ -4,8 +4,12 @@ import requests
 # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
 #            }
 
+lnartist = []
+lnauthor = []
+lnopub = []
 
-def getLNData(url, headers):
+
+def getLNData(url, headers, title):
     link_r = requests.get(url, headers=headers)
     print("\nStatus:", link_r.status_code)
     link_soup = BeautifulSoup(link_r.text, "lxml")
@@ -27,26 +31,38 @@ def getLNData(url, headers):
             "div", {"id": "showepublisher"}).text.strip()
         data_statuscoo = data.find(
             "div", {"id": "editstatus"}).text.strip()
-        print("\nType:", data_type)
-        if len(data_author) > 1:
-            print("Author(s):")
-            for authors in data_author:
-                author = authors.text
-                print(author, " ")
-        if len(data_artist) > 1:
-            print("Artist(s):")
-            for artists in data_artist:
-                artist = artists.text
-                print(artist, " ")
-        print("Licensed:", data_licensed)
-        print("Completely Translated:", data_comp_tl)
-        if len(data_ori_pub) > 1:
-            print("Original Publisher(s):")
-            for opubs in data_ori_pub:
-                opub = opubs.text
-                print(opub, " ")
-        print("English Publisher:", data_eng_pub)
-        print("Status in COO:", data_statuscoo)
+
+    print("\nType:", data_type)
+    # if len(data_author) > 1:
+    print("Author(s):")
+    for authors in data_author:
+        author = authors.text
+        lnauthor.append(author)
+        print(author, " ")
+    print("Artist(s):")
+    for artists in data_artist:
+        artist = artists.text
+        lnartist.append(artist)
+        print(artist, " ")
+    print("Licensed:", data_licensed)
+    print("Completely Translated:", data_comp_tl)
+    print("Original Publisher(s):")
+    for opubs in data_ori_pub:
+        opub = opubs.text
+        lnopub.append(opub)
+        print(opub, " ")
+    print("English Publisher:", data_eng_pub)
+    print("Status in COO:", data_statuscoo)
+    data = {"Title": title,
+            "Type": data_type,
+            "Author(s)": lnauthor,
+            "Artist(s)": lnartist,
+            "Licensed": data_licensed,
+            "Completely Translated": data_comp_tl,
+            "Original Publisher(s)": lnopub,
+            "English Publisher": data_eng_pub,
+            "Status in COO": data_statuscoo}
+    return data
 
 
 # getLNData('https://www.novelupdates.com/series/100-things-i-dont-know-about-my-senior/', headers)
